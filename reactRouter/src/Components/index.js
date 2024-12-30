6f00c3454ba3094362cb6e15c86781efb917206b
thoratpritesh1 <thoratpritesh1@gmail.com>
2024-12-30 11:17:32 +0000

    React Router Project

---
+++ reactRouter/src/Components/index.js
@@ -0,0 +1,9 @@
+import Footer from './Footer.jsx';
+import Header from './Header.jsx';
+import Home from './Home.jsx';
+import About from './About.jsx';
+import Contact from './Contact.jsx';
+import Github, { githubInfoLoader } from './Github.jsx';
+import User from './User.jsx';
+
+export { Footer, Header, Home, About, Contact, Github, githubInfoLoader, User };
---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import { Home, About, Contact, Github, githubInfoLoader, User } from './Components/index.js';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout/>}>
+      <Route path="" element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="contact" element={<Contact/>}/>
+      <Route path="github" element={<Github/>} loader={githubInfoLoader}/>
+      <Route path="user/:userid" element={<User/>}/>
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
